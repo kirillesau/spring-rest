528348461d3d2776bff8af8c1487453aa37ff62c
Kirill Esau <>
2022-07-31 09:15:33 +0200

    Tests

--- src/test/java/com/example/springrest/FriendServiceTest.java
+++ src/test/java/com/example/springrest/FriendServiceTest.java
@@ -7,9 +7,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class FriendServiceTest {
@@ -47,6 +48,75 @@ class FriendServiceTest {
     // then
     assertThat(actual.size()).isEqualTo(5);
     assertThat(actual).isEqualTo(friends);
+  }
+
+  @Test
+  void getFriend_with_non_existing_id_should_return_null() {
+    // given
+    // when
+    final Friend actual = friendService.getFriend(1L);
+
+    // then
+    assertThat(actual).isNull();
+  }
+
+  @Test
+  void getFriend_with_existing_id_should_return_friend() {
+    // given
+    final Friend friend = FriendFactory.createDefaultFriend();
+    when(friendRepository.findById(friend.getId())).thenReturn(Optional.of(friend));
+
+    // when
+    final Friend actual = friendService.getFriend(friend.getId());
+
+    // then
+    assertThat(actual).isEqualTo(friend);
+  }
+
+  @Test
+  void addFriend_with_existing_friend_id_should_update_friend() {
+    // given
+    final Friend friend = FriendFactory.createDefaultFriend();
+    final long generatedId = friend.getId();
+    final Friend oldFriendWithSameId = new Friend(generatedId, "oldFirstname", "oldLastname");
+
+    when(friendRepository.findById(generatedId)).thenReturn(Optional.of(oldFriendWithSameId));
+
+    // when
+    final Friend actual = friendService.addFriend(friend, generatedId);
+
+    // then
+    assertThat(actual).isEqualTo(friend);
+    verify(friendRepository).findById(friend.getId());
+    verify(friendRepository).save(friend);
+    verifyNoMoreInteractions(friendRepository);
+  }
+
+  @Test
+  void addFriend_with_non_existing_friend_id_should_add_to_repository() {
+    // given
+    final Friend friend = FriendFactory.createDefaultFriend();
+    when(friendRepository.findById(5L)).thenReturn(Optional.empty());
+
+    // when
+    final Friend actual = friendService.addFriend(friend, 5L);
+
+    // then
+    assertThat(actual).isEqualTo(friend);
+    verify(friendRepository).findById(5L);
+    verify(friendRepository).save(friend);
+    verifyNoMoreInteractions(friendRepository);
+  }
+
+  @Test
+  void deleteFriend_with_id_should_delete_friend() {
+    // given
+    // when
+    friendService.deleteFriend(1L);
+
+    // then
+    verify(friendRepository).deleteById(1L);
+    verifyNoMoreInteractions(friendRepository);
   }
 
 }
